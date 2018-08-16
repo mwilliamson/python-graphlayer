ca960b85b6251b5543ae44371d67ca80341ad9c3
Michael Williamson <mike@zwobble.org>
2018-08-16 19:10:03 +0100

    Add test for core module

---
+++ tests/test_core.py
@@ -0,0 +1,18 @@
+from precisely import assert_that, equal_to
+
+import graphlayer.core as g
+
+
+def test_given_expander_has_no_dependencies_when_expand_is_called_with_no_dependencies_then_expander_is_directly_called():
+    @g.expander("root", "integer_representation")
+    def expand_root(graph, query):
+        return 42
+    
+    expanders = [expand_root]
+    
+    class Query(object):
+        type = "root"
+    
+    result = g.create_graph(expanders).expand(Query, "integer_representation")
+    
+    assert_that(result, equal_to(42))
