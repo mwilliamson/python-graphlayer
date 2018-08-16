54be3f12e4b0c28c59264deee7f7a1584aabc593
Michael Williamson <mike@zwobble.org>
2018-08-16 19:13:11 +0100

    Test graph argument of expanders

--- tests/test_core.py
+++ tests/test_core.py
@@ -32,3 +32,26 @@ def test_when_expand_is_called_then_expander_is_passed_query():
     result = g.create_graph(expanders).expand(Query, "integer_representation")
     
     assert_that(result, equal_to(42))
+
+
+def test_when_expand_is_called_then_expander_is_passed_the_graph():
+    @g.expander("root", "integer_representation")
+    def expand_root(graph, query):
+        return graph.expand(LeafQuery, "integer_representation")
+    
+    @g.expander("leaf", "integer_representation")
+    def expand_leaf(graph, query):
+        return query.value
+    
+    expanders = [expand_root, expand_leaf]
+    
+    class RootQuery(object):
+        type = "root"
+    
+    class LeafQuery(object):
+        type = "leaf"
+        value = 47
+    
+    result = g.create_graph(expanders).expand(RootQuery, "integer_representation")
+    
+    assert_that(result, equal_to(47))
