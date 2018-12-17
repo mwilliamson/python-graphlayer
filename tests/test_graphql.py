65e2ef490dae17aacb722f574bf4a4b95eb60587
Michael Williamson <mike@zwobble.org>
2018-12-17 13:54:10 +0000

    Check fields in query

--- tests/test_graphql.py
+++ tests/test_graphql.py
@@ -24,7 +24,10 @@ def test_simple_query_is_converted_to_object_query():
     assert_that(object_query, is_object_query(
         type=Root,
         fields=is_mapping({
-            "one": is_field_query(type_query=schema.scalar_query),
+            "one": is_field_query(
+                field=is_field(name="one"),
+                type_query=schema.scalar_query,
+            ),
         }),
     ))
 
@@ -48,7 +51,10 @@ def test_fields_can_have_alias():
     assert_that(object_query, is_object_query(
         type=Root,
         fields=is_mapping({
-            "value": is_field_query(type_query=schema.scalar_query),
+            "value": is_field_query(
+                field=is_field(name="one"),
+                type_query=schema.scalar_query,
+            ),
         }),
     ))
 
@@ -72,7 +78,10 @@ def test_field_names_are_converted_to_snake_case():
     assert_that(object_query, is_object_query(
         type=Root,
         fields=is_mapping({
-            "oneValue": is_field_query(field=is_field(name="one_value"), type_query=schema.scalar_query),
+            "oneValue": is_field_query(
+                field=is_field(name="one_value"),
+                type_query=schema.scalar_query,
+            ),
         }),
     ))
 
