14cf1e57c927887a6019ffabadbe67704bec4ec0
Michael Williamson <mike@zwobble.org>
2018-12-23 14:53:19 +0000

    Add execute() to graphql package

--- graphlayer/graphql/__init__.py
+++ graphlayer/graphql/__init__.py
@@ -1,1 +1,14 @@
+from . import parser
 
+
+def execute(*, graph, query_type, mutation_type=None, document_text, variables=None):
+    query = parser.document_text_to_query(
+        document_text=document_text,
+        query_type=query_type,
+        mutation_type=mutation_type,
+        variables=variables,
+    )
+    
+    result = graph.resolve(query)
+    
+    return query.to_json_value(result)
