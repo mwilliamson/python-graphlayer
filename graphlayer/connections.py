64aafad46bc29f2147b7a8d5add275a9265d3f9a
Michael Williamson <mike@zwobble.org>
2019-06-19 22:27:49 +0100

    Add connections module

---
+++ graphlayer/connections.py
@@ -0,0 +1,131 @@
+import base64
+
+import graphlayer as g
+from graphlayer.core import Injector
+
+
+def forward_connection(*, connection_type_name, node_type, select_by_cursor, fetch_cursors, cursor_encoding):
+    Connection = g.ObjectType(
+        connection_type_name,
+        fields=lambda: (
+            g.field("edges", type=g.ListType(Edge)),
+            g.field("page_info", type=PageInfo),
+        ),
+    )
+
+    Edge = g.ObjectType(
+        node_type.name + "Edge",
+        fields=lambda: (
+            g.field("cursor", type=g.String),
+            g.field("node", type=node_type),
+        ),
+    )
+
+    class ConnectionQuery(object):
+        @staticmethod
+        def select_field(query, *, args):
+            return ConnectionQuery(type_query=query, first=args.first, after=args.after)
+
+        def __init__(self, *, type_query, first, after):
+            self.type = ConnectionQuery
+            self.type_query = type_query
+            self.first = first
+            self.after = after
+
+    @g.dependencies(injector=Injector)
+    @g.resolver(ConnectionQuery)
+    def resolve_connection(graph, query, *, injector):
+        build_connection = g.create_object_builder(query.type_query)
+
+        if query.after is None:
+            after_cursor = None
+        else:
+            after_cursor = cursor_encoding.decode(query.after)
+
+        edge_cursors = injector.call_with_dependencies(fetch_cursors, after_cursor=after_cursor, limit=query.first + 1)
+        if len(edge_cursors) > query.first:
+            edge_cursors = edge_cursors[:-1]
+            has_next_page = True
+        else:
+            has_next_page = False
+
+        @build_connection.field(Connection.fields.edges)
+        def field_edges(field_query):
+            build_edge = g.create_object_builder(field_query.type_query.element_query)
+
+            @build_edge.getter(Edge.fields.cursor)
+            def field_cursor(cursor):
+                return cursor_encoding.encode(cursor)
+
+            @build_edge.field(Edge.fields.node)
+            def field_node(field_query):
+                edges = graph.resolve(
+                    select_by_cursor(field_query.type_query, edge_cursors)
+                )
+
+                return lambda edge_cursor: edges[edge_cursor]
+
+            return lambda _: [
+                build_edge(edge_cursor)
+                for edge_cursor in edge_cursors
+            ]
+
+        @build_connection.field(Connection.fields.page_info)
+        def field_page_info(field_query):
+            build_page_info = g.create_object_builder(field_query.type_query)
+
+            @build_page_info.getter(PageInfo.fields.has_next_page)
+            def field_has_next_page(_):
+                return has_next_page
+
+            @build_page_info.getter(PageInfo.fields.end_cursor)
+            def field_end_cursor(_):
+                if edge_cursors:
+                    return cursor_encoding.encode(edge_cursors[-1])
+                else:
+                    return None
+
+            return lambda _: build_page_info(None)
+
+        return build_connection(None)
+
+    return ForwardConnection(
+        Connection=Connection,
+        Edge=Edge,
+        resolver=resolve_connection,
+        select_field=ConnectionQuery.select_field,
+    )
+
+
+class ForwardConnection(object):
+    def __init__(self, Connection, Edge, resolver, select_field):
+        self.Connection = Connection
+        self.Edge = Edge
+        self.resolvers = (resolver, )
+        self.select_field = select_field
+
+    def field(self, field_name):
+        return g.field(field_name, type=self.Connection, params=(
+            g.param("after", type=g.NullableType(g.String), default=None),
+            g.param("first", type=g.Int),
+        ))
+
+
+PageInfo = g.ObjectType(
+    "PageInfo",
+    fields=lambda: (
+        g.field("end_cursor", type=g.NullableType(g.String)),
+        g.field("has_next_page", type=g.Boolean),
+    ),
+)
+
+
+class int_cursor_encoding(object):
+    @staticmethod
+    def encode(cursor):
+        return base64.b64encode(str(cursor).encode("ascii")).decode("ascii")
+
+    @staticmethod
+    def decode(cursor):
+        return int(base64.b64decode(cursor.encode("ascii")).decode("ascii"))
+
---
+++ tests/test_connections.py
@@ -0,0 +1,304 @@
+from precisely import assert_that, contains_exactly, has_attrs
+
+import graphlayer as g
+import graphlayer.connections
+
+
+Book = g.ObjectType(
+    "Book",
+    fields=lambda: (
+        g.field("title", type=g.String),
+    ),
+)
+
+
+def select_books_by_id(query, ids):
+    return BookQuery(type_query=query, ids=ids)
+
+
+class BookQuery(object):
+    def __init__(self, type_query, ids):
+        self.type = BookQuery
+        self.type_query = type_query
+        self.ids = ids
+
+
+@g.resolver(BookQuery)
+@g.dependencies(books="all_books")
+def resolve_books(graph, query, books):
+    build_book_object = g.create_object_builder(query.type_query)
+
+    @build_book_object.getter(Book.fields.title)
+    def field_title(book):
+        return book
+
+    return {
+        book_id: build_book_object(books[book_id])
+        for book_id in query.ids
+    }
+
+
+@g.dependencies(books="all_books")
+def fetch_book_cursors(*, after_cursor, limit, books):
+    return [
+        book_id
+        for book_id, book in books.items()
+        if after_cursor is None or book_id > after_cursor
+    ][:limit]
+
+
+books_connection = graphlayer.connections.forward_connection(
+    connection_type_name="BooksConnection",
+    node_type=Book,
+    select_by_cursor=select_books_by_id,
+    cursor_encoding=graphlayer.connections.int_cursor_encoding,
+    fetch_cursors=fetch_book_cursors,
+)
+
+BooksConnection = books_connection.Connection
+BookEdge = books_connection.Edge
+PageInfo = graphlayer.connections.PageInfo
+
+Query = g.ObjectType(
+    "Query",
+    fields=lambda: (
+        books_connection.field("books_connection"),
+    ),
+)
+
+resolve_query = g.root_object_resolver(Query)
+
+@resolve_query.field(Query.fields.books_connection)
+def resolve_query_field_books_connection(graph, query, args):
+    return graph.resolve(books_connection.select_field(query, args=args))
+
+resolvers = (resolve_books, books_connection.resolvers, resolve_query)
+
+graph_definition = g.define_graph(resolvers)
+
+def create_graph(books):
+    return graph_definition.create_graph({"all_books": books})
+
+
+def test_when_there_are_no_edges_then_connection_is_empty():
+    graph = create_graph({})
+
+    result = graph.resolve(
+        Query(
+            g.key("books", Query.fields.books_connection(
+                Query.fields.books_connection.params.first(2),
+
+                g.key("edges", BooksConnection.fields.edges(
+                    g.key("node", BookEdge.fields.node(
+                        g.key("title", Book.fields.title()),
+                    )),
+                )),
+                g.key("page_info", BooksConnection.fields.page_info(
+                    g.key("end_cursor", PageInfo.fields.end_cursor()),
+                    g.key("has_next_page", PageInfo.fields.has_next_page()),
+                )),
+            )),
+        )
+    )
+
+    assert_that(result.books, has_attrs(
+        edges=contains_exactly(),
+        page_info=has_attrs(end_cursor=None, has_next_page=False),
+    ))
+
+
+def test_when_there_are_more_edges_than_requested_then_first_edges_are_fetched():
+    graph = create_graph({
+        42: "Leave it to Psmith",
+        43: "The Gentleman's Guide to Vice and Virtue",
+        44: "Catch-22",
+    })
+
+    result = graph.resolve(
+        Query(
+            g.key("books", Query.fields.books_connection(
+                Query.fields.books_connection.params.first(2),
+
+                g.key("edges", BooksConnection.fields.edges(
+                    g.key("node", BookEdge.fields.node(
+                        g.key("title", Book.fields.title()),
+                    )),
+                )),
+                g.key("page_info", BooksConnection.fields.page_info(
+                    g.key("has_next_page", PageInfo.fields.has_next_page()),
+                )),
+            )),
+        )
+    )
+
+    assert_that(result.books, has_attrs(
+        edges=contains_exactly(
+            has_attrs(node=has_attrs(title="Leave it to Psmith")),
+            has_attrs(node=has_attrs(title="The Gentleman's Guide to Vice and Virtue")),
+        ),
+        page_info=has_attrs(has_next_page=True),
+    ))
+
+
+def test_when_there_are_fewer_edges_than_requested_then_all_edges_are_fetched():
+    graph = create_graph({
+        42: "Leave it to Psmith",
+    })
+
+    result = graph.resolve(
+        Query(
+            g.key("books", Query.fields.books_connection(
+                Query.fields.books_connection.params.first(2),
+
+                g.key("edges", BooksConnection.fields.edges(
+                    g.key("node", BookEdge.fields.node(
+                        g.key("title", Book.fields.title()),
+                    )),
+                )),
+                g.key("page_info", BooksConnection.fields.page_info(
+                    g.key("has_next_page", PageInfo.fields.has_next_page()),
+                )),
+            )),
+        )
+    )
+
+    assert_that(result.books, has_attrs(
+        edges=contains_exactly(
+            has_attrs(node=has_attrs(title="Leave it to Psmith")),
+        ),
+        page_info=has_attrs(has_next_page=False),
+    ))
+
+
+def test_when_there_are_exactly_number_of_edges_requested_then_all_edges_are_fetched():
+    graph = create_graph({
+        42: "Leave it to Psmith",
+        43: "The Gentleman's Guide to Vice and Virtue",
+    })
+
+    result = graph.resolve(
+        Query(
+            g.key("books", Query.fields.books_connection(
+                Query.fields.books_connection.params.first(2),
+
+                g.key("edges", BooksConnection.fields.edges(
+                    g.key("node", BookEdge.fields.node(
+                        g.key("title", Book.fields.title()),
+                    )),
+                )),
+                g.key("page_info", BooksConnection.fields.page_info(
+                    g.key("has_next_page", PageInfo.fields.has_next_page()),
+                )),
+            )),
+        )
+    )
+
+    assert_that(result.books, has_attrs(
+        edges=contains_exactly(
+            has_attrs(node=has_attrs(title="Leave it to Psmith")),
+            has_attrs(node=has_attrs(title="The Gentleman's Guide to Vice and Virtue")),
+        ),
+        page_info=has_attrs(has_next_page=False),
+    ))
+
+
+def test_end_cursor_can_be_used_as_after_argument_to_get_next_page():
+    graph = create_graph({
+        42: "Leave it to Psmith",
+        43: "The Gentleman's Guide to Vice and Virtue",
+        44: "Catch-22",
+        45: "Pericles, Prince of Tyre",
+        46: "Captain Corelli's Mandolin",
+    })
+
+    def fetch(*, after):
+        return graph.resolve(
+            Query(
+                g.key("books", Query.fields.books_connection(
+                    Query.fields.books_connection.params.first(2),
+                    Query.fields.books_connection.params.after(after),
+
+                    g.key("edges", BooksConnection.fields.edges(
+                        g.key("node", BookEdge.fields.node(
+                            g.key("title", Book.fields.title()),
+                        )),
+                    )),
+                    g.key("page_info", BooksConnection.fields.page_info(
+                        g.key("end_cursor", PageInfo.fields.end_cursor()),
+                        g.key("has_next_page", PageInfo.fields.has_next_page()),
+                    )),
+                )),
+            )
+        )
+
+    result = fetch(after=None)
+    assert_that(result.books, has_attrs(
+        edges=contains_exactly(
+            has_attrs(node=has_attrs(title="Leave it to Psmith")),
+            has_attrs(node=has_attrs(title="The Gentleman's Guide to Vice and Virtue")),
+        ),
+        page_info=has_attrs(has_next_page=True),
+    ))
+
+    result = fetch(after=result.books.page_info.end_cursor)
+    assert_that(result.books, has_attrs(
+        edges=contains_exactly(
+            has_attrs(node=has_attrs(title="Catch-22")),
+            has_attrs(node=has_attrs(title="Pericles, Prince of Tyre")),
+        ),
+        page_info=has_attrs(has_next_page=True),
+    ))
+
+    result = fetch(after=result.books.page_info.end_cursor)
+    assert_that(result.books, has_attrs(
+        edges=contains_exactly(
+            has_attrs(node=has_attrs(title="Captain Corelli's Mandolin")),
+        ),
+        page_info=has_attrs(has_next_page=False),
+    ))
+
+    result = fetch(after=result.books.page_info.end_cursor)
+    assert_that(result.books, has_attrs(
+        edges=contains_exactly(),
+        page_info=has_attrs(end_cursor=None, has_next_page=False),
+    ))
+
+
+def test_edge_cursor_can_be_used_as_after_argument_to_get_next_page():
+    graph = create_graph({
+        42: "Leave it to Psmith",
+        43: "The Gentleman's Guide to Vice and Virtue",
+        44: "Catch-22",
+    })
+
+    def fetch(*, after):
+        return graph.resolve(
+            Query(
+                g.key("books", Query.fields.books_connection(
+                    Query.fields.books_connection.params.first(2),
+                    Query.fields.books_connection.params.after(after),
+
+                    g.key("edges", BooksConnection.fields.edges(
+                        g.key("cursor", BookEdge.fields.cursor()),
+                        g.key("node", BookEdge.fields.node(
+                            g.key("title", Book.fields.title()),
+                        )),
+                    )),
+                )),
+            )
+        )
+
+    result = fetch(after=None)
+    assert_that(result.books, has_attrs(
+        edges=contains_exactly(
+            has_attrs(node=has_attrs(title="Leave it to Psmith")),
+            has_attrs(node=has_attrs(title="The Gentleman's Guide to Vice and Virtue")),
+        ),
+    ))
+
+    result = fetch(after=result.books.edges[-1].cursor)
+    assert_that(result.books, has_attrs(
+        edges=contains_exactly(
+            has_attrs(node=has_attrs(title="Catch-22")),
+        ),
+    ))
