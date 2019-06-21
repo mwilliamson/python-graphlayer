3a39e4dcdbfe47b8b0843472060e0a735e752116
Michael Williamson <mike@zwobble.org>
2019-06-21 20:26:53 +0100

    Bump version to 0.2.3

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def read(fname):
 
 setup(
     name='graphlayer',
-    version='0.2.2',
+    version='0.2.3',
     description='',
     long_description=read("README.rst"),
     author='Michael Williamson',
